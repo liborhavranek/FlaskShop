188a5da9cdf578efaf44ca4647bb6d05eca1cd6c
ctiborekskutr <liborhavranek91@gmail.com>
2023-03-23 23:02:03 +0100

    Make work scss
    - add tests
    - implement all scss file
    
    see #1

--- myshop/tests/test_myshopapp.py
+++ myshop/tests/test_myshopapp.py
@@ -1,4 +1,8 @@
 import unittest
+
+from flask_assets import Environment, Bundle
+from webassets.filter import get_filter
+
 from myshop import create_app
 
 
@@ -8,6 +12,27 @@ class TestCreateApp(unittest.TestCase):
         self.app = create_app()
         self.app.testing = True
         self.client = self.app.test_client()
+
+    def set_bundles(self):
+        self.assets = Environment(self.app)
+        self.bundles = {
+            'index_style': Bundle(
+                'SCSS/index.scss',
+                filters='libsass',
+                output='Gen/index.css',
+            ),
+            'register_style': Bundle(
+                'SCSS/register.scss',
+                filters='libsass',
+                output='Gen/register.css',
+            ),
+            'product_style': Bundle(
+                'SCSS/product.scss',
+                filters='libsass',
+                output='Gen/product.css',
+            )
+        }
+        self.assets.register(self.bundles)
 
     def test_create_app_have_secret_key(self):
         # Test that the app has the expected configuration
@@ -47,7 +72,44 @@ class TestCreateApp(unittest.TestCase):
             response = client.get('/shutdown')
             self.assertEqual(response.status_code, 404)
 
-# TODO write tests for bundless
+    def test_index_style_is_registered_in_bundles(self):
+        self.set_bundles()
+        registered_bundles = self.assets._named_bundles.keys()
+        self.assertIn("index_style", registered_bundles)
+
+    def test_register_style_is_registered_in_bundles(self):
+        self.set_bundles()
+        registered_bundles = self.assets._named_bundles.keys()
+        self.assertIn("register_style", registered_bundles)
+
+    def test_product_style_is_registered_in_bundles(self):
+        self.set_bundles()
+        registered_bundles = self.assets._named_bundles.keys()
+        self.assertIn("product_style", registered_bundles)
+
+    def test_libsass_filter_is_used_for_index_style(self):
+        self.set_bundles()
+        bundle = self.assets['index_style']
+        for filter_name in bundle.filters:
+            if filter_name == 'libsass':
+                sass_filter = get_filter(filter_name)
+                self.assertIsNotNone(sass_filter)
+
+    def test_libsass_filter_is_used_for_register_style(self):
+        self.set_bundles()
+        bundle = self.assets['register_style']
+        for filter_name in bundle.filters:
+            if filter_name == 'libsass':
+                sass_filter = get_filter(filter_name)
+                self.assertIsNotNone(sass_filter)
+
+    def test_libsass_filter_is_used_for_product_style(self):
+        self.set_bundles()
+        bundle = self.assets['product_style']
+        for filter_name in bundle.filters:
+            if filter_name == 'libsass':
+                sass_filter = get_filter(filter_name)
+                self.assertIsNotNone(sass_filter)
 
 
 if __name__ == '__main__':
