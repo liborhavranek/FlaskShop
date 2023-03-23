aaeb0229678c0ee679e5f0095c6f7c7e1eb7d759
ctiborekskutr <liborhavranek91@gmail.com>
2023-03-23 18:49:11 +0100

    try run tests again on github unit test

--- myshop/auth.py
+++ myshop/auth.py
@@ -2,7 +2,6 @@ from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__, template_folder='templates/authenticates')
 
-
 @auth.route('/')
 def authenticate():
     return render_template('auth.html')
