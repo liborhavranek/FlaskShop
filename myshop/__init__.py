d1cec90d020c1b917bd9d371d443fc2d86cc3cd0
ctiborekskutr <liborhavranek91@gmail.com>
2023-03-23 21:11:46 +0100

    Make work scss
    - add base template
    - make work scss

--- myshop/__init__.py
+++ myshop/__init__.py
@@ -1,10 +1,31 @@
 from flask import Flask
+from flask_assets import Environment, Bundle
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secret_key'
 
+    assets = Environment(app)
+    bundles = {  # define nested Bundle
+        'index_style': Bundle(
+            'SCSS/index.scss',
+            filters='libsass',
+            output='Gen/index.css',
+        ),
+        'register_style': Bundle(
+            'SCSS/register.scss',
+            filters='libsass',
+            output='Gen/register.css',
+        ),
+        'product_style': Bundle(
+            'SCSS/product.scss',
+            filters='libsass',
+            output='Gen/product.css',
+        )
+    }
+    assets.register(bundles)
+    
     from .admin import admin
     from .products import products
     from .views import views
