0aa7d0ac1d121d89c75feb8138567e0ff3fbbfb6
ctiborekskutr <liborhavranek91@gmail.com>
2023-03-25 21:42:26 +0100

    Add new field to costumer model
    - add fields
    
    see #6

--- myshop/models.py
+++ myshop/models.py
@@ -12,3 +12,30 @@ class Customer(db.Model, UserMixin):
     phone_code = db.Column(db.String(50), unique=False)
     phone = db.Column(db.String(50), unique=False)
     password = db.Column(db.String(200), unique=False)
+
+    # --------------- Fakturacni udaje ---------------------
+    faktura_first_name = db.Column(db.String(50), unique=False, nullable=True)
+    faktura_last_name = db.Column(db.String(50), unique=False, nullable=True)
+    faktura_city = db.Column(db.String(50), unique=False, nullable=True)
+    faktura_street = db.Column(db.String(50), unique=False, nullable=True)
+    faktura_zipcode = db.Column(db.String(50), unique=False, nullable=True)
+
+    # ---------------Dodaci udaje ---------------------------------
+
+    dodej_first_name = db.Column(db.String(50), unique=False, nullable=True)
+    dodej_last_name = db.Column(db.String(50), unique=False, nullable=True)
+    dodej_company = db.Column(db.String(50), unique=False, nullable=True)
+    dodej_city = db.Column(db.String(50), unique=False, nullable=True)
+    dodej_street = db.Column(db.String(50), unique=False, nullable=True)
+    dodej_zipcode = db.Column(db.String(50), unique=False, nullable=True)
+    dodej_info = db.Column(db.String(50), unique=False, nullable=True)
+    dodej_phone_code = db.Column(db.String(50), unique=False, nullable=True)
+    dodej_phone = db.Column(db.String(50), unique=False, nullable=True)
+
+    # -------------------Firemní údaje -------------------------------------
+
+    firma_ico = db.Column(db.String(50), unique=False, nullable=True)
+    firma_dic = db.Column(db.String(50), unique=False, nullable=True)
+    firma_bank_acc = db.Column(db.String(50), unique=False, nullable=True)
+    firma_bank_number = db.Column(db.String(50), unique=False, nullable=True)
+    firma_spec_symbol = db.Column(db.String(50), unique=False, nullable=True)
