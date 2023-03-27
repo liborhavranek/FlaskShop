d6197ba85f48c64083735f26a1c0c28e302b9412
ctiborekskutr <liborhavranek91@gmail.com>
2023-03-28 01:48:09 +0200

    Add new field to costumer model
    - change mixin
    
    see #12

--- myshop/tests/my_test_mixin.py
+++ myshop/tests/my_test_mixin.py
@@ -5,15 +5,19 @@ import time
 class TestMixin:
     def start_test(self):
         self.start_time = time.monotonic()
-        print("Starting test...............................................")
-        time.sleep(0.2)  # wait for 1 second
+        print("\033[91mStarting test...............................................\033[0m")
+#        time.sleep(0.1)
         os.environ.pop('FLASK_ENV', None)
+
+        print(
+            f"Running test: {self.test_name} - "
+            f"{self._testMethodName} - {time.monotonic() - self.start_time:.3f} seconds")
+ #       time.sleep(0.1)
+        print("\033[32m" + f"{self._testMethodName} - completed.\n\n" + "\033[0m")
+        time.sleep(0.1)
 
     def setUp(self):
         super().setUp()
         self.start_test()
 
-        print(
-            f"Running test: {self.test_name} - "
-            f"{self._testMethodName} - {time.monotonic() - self.start_time:.3f} seconds")
-        print(f"{self._testMethodName} - completed.\n\n")
+
