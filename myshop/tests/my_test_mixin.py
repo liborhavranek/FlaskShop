ce6868ca38795b15fa5b4e200f7a0fd5ca9429ff
ctiborekskutr <liborhavranek91@gmail.com>
2023-04-01 23:54:07 +0200

    Add new field to costumer model
    - add tests
    
    see #12

--- myshop/tests/my_test_mixin.py
+++ myshop/tests/my_test_mixin.py
@@ -8,7 +8,7 @@ class TestMixin:
     def start_test(self):
         self.start_time = time.monotonic()
         print("\033[91mStarting test......................................................................\033[0m")
-        time.sleep(0.2)
+        # time.sleep(0.2)
         total_width = 70  # Set the total width of the progress bar
         for i in range(8):
             percent_complete = (i + 1) * 12.5
@@ -16,7 +16,7 @@ class TestMixin:
             progress_bar = "#" * num_hashes + "." * (total_width - num_hashes)
             sys.stdout.write(f"\rTesting {progress_bar} {percent_complete:>3} %")
             sys.stdout.flush()
-            time.sleep(0.1)
+            # time.sleep(0.1)
         os.environ.pop('FLASK_ENV', None)
 
         if self._outcome.success:
@@ -38,7 +38,7 @@ class TestMixin:
             print("\033[31m" + f"{self._testMethodName} - failed." + "\033[0m")
             print("\033[31m" + f"{self._testMethodName} - completed.\n\n" + "\033[0m")
 
-        time.sleep(0.8)
+        # time.sleep(0.8)
 
 
     def setUp(self):
