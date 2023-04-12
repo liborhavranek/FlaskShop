ba2340c9ab1450b57d9eed59908c531c08efe557
ctiborekskutr <liborhavranek91@gmail.com>
2023-04-12 15:46:38 +0200

    Add new style for login form
    - login page is formated
    
    see #13

--- myshop/static/js/registration_form.js
+++ myshop/static/js/registration_form.js
@@ -446,11 +446,20 @@ $(".registration-firma-spec-symbol-input").on('input', function() {
 const toggleBtn1 = document.querySelector('#toggleBtn1');
 const collapseDiv1 = document.querySelector('#collapseDiv1');
 toggleBtn1.addEventListener('click', function() {
+  collapseDiv1.classList.remove('collapse');
   collapseDiv1.classList.toggle('collapse');
 });
 
 const toggleBtn2 = document.querySelector('#toggleBtn2');
 const collapseDiv2 = document.querySelector('#collapseDiv2');
 toggleBtn2.addEventListener('click', function() {
+  collapseDiv2.classList.remove('collapse');
   collapseDiv2.classList.toggle('collapse');
+});
+
+const toggleBtn3 = document.querySelector('#toggleBtn3');
+const collapseDiv3 = document.querySelector('#collapseDiv3');
+toggleBtn3.addEventListener('click', function() {
+  collapseDiv3.classList.remove('collapse');
+  collapseDiv3.classList.toggle('collapse');
 });
--- myshop/tests/tests_templates/test_login_template.py
+++ myshop/tests/tests_templates/test_login_template.py
@@ -82,3 +82,77 @@ class TestRegisterTemplateAllTemplates(TestMixin, unittest.TestCase):
         soup = BeautifulSoup(response.data, 'html.parser')
         navbar = soup.find('nav', class_='navbar')
         self.assertIsNotNone(navbar)
+
+
+class TestAuthTemplateOnlyLoginTemplate(TestMixin, unittest.TestCase):
+    """Test what are specific only for this template"""
+
+    @classmethod
+    def setUpClass(cls):
+        cls.test_name = cls.__name__
+
+    def setUp(self):
+        self.app = create_app()
+        self.app.testing = True
+        self.client = self.app.test_client()
+        super().setUp()
+
+    def test_register_form_have_closed_form_tag(self):
+        response = self.client.get('/auth/login', follow_redirects=True)
+        self.assertIn(b'<form method="POST">', response.data)
+        self.assertIn(b'</form>', response.data)
+
+    def test_register_form_have_all_input_fields(self):
+        fields_to_test = [
+            'email', 'password'
+                          ]
+
+        response = self.client.get('/auth/login', follow_redirects=True)
+        soup = BeautifulSoup(response.data, 'html.parser')
+        form_tag = soup.find('form', {'method': 'POST'})
+        form_input_fields = [input_tag['name'] for input_tag in form_tag.find_all('input')]
+
+        for field in fields_to_test:
+            with self.subTest(field=field):
+
+                self.assertIn(field, form_input_fields)
+
+    def test_register_form_have_all_select_fields(self):
+        fields_to_test = [
+            'phone_code', 'dodej_phone_code'
+        ]
+
+        response = self.client.get('/auth/register', follow_redirects=True)
+        soup = BeautifulSoup(response.data, 'html.parser')
+        form_tag = soup.find('form', {'method': 'POST'})
+        form_select_fields = [select_tag['name'] for select_tag in form_tag.find_all('select')]
+
+        for field in fields_to_test:
+            with self.subTest(field=field):
+                self.assertIn(field, form_select_fields)
+
+    def test_register_form_have_all_labels(self):
+        expected_labels = {
+            'email': 'Email:',
+            'password': 'Heslo:',
+        }
+        response = self.client.get('/auth/login', follow_redirects=True)
+        soup = BeautifulSoup(response.data, 'html.parser')
+        form_labels = {label_tag['for']: label_tag.text.strip() for label_tag in soup.find_all('label')}
+
+        for field, label in expected_labels.items():
+            with self.subTest(field=field):
+                self.assertIn(label, form_labels[field])
+
+    # def test_register_form_has_submit_button(self):
+    #     response = self.client.get('/auth/login', follow_redirects=True)
+    #     soup = BeautifulSoup(response.data, 'html.parser')
+    #
+    #     # Check that the form contains a submit button
+    #     submit_button = soup.find('input', {'type': 'submit', 'value': 'Přihlásit'})
+    #     self.assertIsNotNone(submit_button)
+# TODO: fix that test
+
+
+if __name__ == '__main__':
+    unittest.main()
