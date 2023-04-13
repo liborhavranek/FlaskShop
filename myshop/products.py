1edccc16408e60c14fe8009e216814636ccab560
ctiborekskutr <liborhavranek91@gmail.com>
2023-04-13 13:35:00 +0200

    add brand in the project
    - add tests for add brand page
    
    see #21

--- myshop/products.py
+++ myshop/products.py
@@ -2,9 +2,17 @@
 
 from flask import Blueprint, render_template
 
+from myshop.forms.brand_form import BrandForm
+
 products = Blueprint('products', __name__, template_folder='templates/products')
 
 
 @products.route('/')
 def product() -> str:
     return render_template('products.html')
+
+
+@products.route('/create-brand')
+def create_brand():
+    form = BrandForm()
+    return render_template('add_brand.html', form=form)
