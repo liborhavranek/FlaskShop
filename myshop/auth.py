affaf678c533f35aaf552e91616ada39dca6cb06
ctiborekskutr <liborhavranek91@gmail.com>
2023-04-24 23:33:20 +0200

    add product
    - create user fixed
    
    see #25

--- myshop/auth.py
+++ myshop/auth.py
@@ -116,7 +116,7 @@ def create_test_data():
         customer.username = user["username"]
         customer.email = user["email"]
         customer.phone = user["phone"]
-        customer.password = generate_password_hash(password, method='sha256')
+        customer.user_password = generate_password_hash(password, method='sha256')
         db.session.add(customer)
     db.session.commit()
     return render_template("auth.html", customer=current_user)
