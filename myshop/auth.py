53b885f7f50f499524e584b35c2d2d33b32b21ff
liborhavranek <liborhavranek91@gmai.com>
2023-05-12 13:25:57 +0200

    Create Data
    - save date in db from csv files working good
    
    see #33

--- myshop/auth.py
+++ myshop/auth.py
@@ -1,4 +1,5 @@
 """ Libor Havránek App Copyright (C)  23.3 2023 """
+import csv
 
 from flask import Blueprint, render_template, request, flash, redirect
 from flask_login import login_user, current_user, login_required, logout_user
@@ -136,78 +137,6 @@ def create_test_data():
         {"category_name": "Herní konzole"},
     ]
 
-    products = [
-        {'product_name': 'Iphone 13',
-         "price": 799,
-         "discount": 5,
-         "stock": 20,
-         "size": 6.1,
-         "size_units": "in",
-         "weight": 174,
-         "weight_units": "g",
-         "color": "stříbrná",
-         "subheading": "Nový iPhone 13 - Výkonný a stylový mobilní telefon",
-         "description": "Popis produktu: Nový iPhone 13 přináší vysoký výkon a stylový design. Disponuje 6,1palcovým "
-                        "Liquid Retina displejem s True Tone technologií a špičkovým fotoaparátem, který vám umožní "
-                        "snímat neuvěřitelně detailní fotografie a videa. Procesor A15 Bionic zaručí hladký chod a "
-                        "výkonná baterie vám umožní používat telefon až 19 hodin. iPhone 13 je také odolný vůči "
-                        "vode a prachu a podporuje nejnovější verzi operačního systému iOS.",
-         "brand_id": 1,
-         "category_id": 1,
-         "visit_count": 350,
-         "product_image": "test_image_iphone_13_1.jpeg"
-         },
-
-        {'product_name': 'iPhone 13 Pro',
-         'price': 899,
-         'discount': 5,
-         'stock': 100,
-         'size': 6.1,
-         'size_units': 'in',
-         'weight': 204,
-         'weight_units': 'g',
-         'color': 'stříbrná',
-         'subheading': 'Vylepšený iPhone 13 Pro - Profesionální výkon a funkce',
-         'description': 'Popis produktu: iPhone 13 Pro je nejnovější vlajkovou lodí značky Apple. Tento mobilní telefon'
-                        ' nabízí profesionální výkon a funkce, díky kterým můžete svůj život zlepšit. Telefon má velký'
-                        ' 6,1palcový Super Retina XDR displej s ProMotion technologií, který zobrazuje '
-                        'výrazné a detailní barvy. Díky pokročilému systému fotoaparátů s trojitým objektivem můžete'
-                        ' snímat skvělé fotografie a videa s vysokým rozlišením, a to i v podmínkách s nízkým'
-                        ' osvětlením. Nový procesor A15 Bionic zajišťuje hladký chod a neuvěřitelnou zábavu. '
-                        'Telefon obsahuje také vysoce výkonnou baterii, která umožní až 22 hodin hovoru nebo až 75 '
-                        'hodin poslechu hudby. Nový iPhone 13 Pro je navíc odolný vůči vodě a prachu, takže vás '
-                        'nezklame ani v náročných podmínkách.',
-         'brand_id': 1,
-         'category_id': 1,
-         "visit_count": 850,
-         'product_image': 'test_image_iphone_13_pro_1.jpeg'},
-
-        {'product_name': 'Iphone 13 pro Max',
-         "price": 999,
-         "discount": 5,
-         "stock": 10,
-         "size": 6.7,
-         "size_units": "in",
-         "weight": 238,
-         "weight_units": "g",
-         "color": "cerna",
-         "subheading": "Nový iPhone 13 Pro Max - Výkon a kvalita bez kompromisů",
-         "description": "Popis produktu: Nový iPhone 13 Pro Max je špičkou v oblasti mobilních telefonů, který nabízí"
-                        " nejvyšší výkon a kvalitu bez kompromisů. Disponuje nejmodernějšími technologiemi a funkcemi, "
-                        "které zajistí hladký chod a neuvěřitelnou zábavu. Telefon má velký 6,7palcový"
-                        " Super Retina XDR displej s ProMotion technologií, který zobrazuje výrazné a detailní barvy."
-                        " Díky pokročilému systému fotoaparátů můžete snímat skvělé fotografie a videa s vysokým"
-                        " rozlišením, a to i v podmínkách s nízkým osvětlením. Telefon obsahuje nejnovější procesor"
-                        " A15 Bionic a vysoce výkonnou baterii, která umožní až 28 hodin hovoru nebo až 95 hodin"
-                        " poslechu hudby. Nový iPhone 13 Pro Max je navíc odolný vůči vodě a prachu, takže vás"
-                        " nezklame ani v náročných podmínkách.",
-         "brand_id": 1,
-         "category_id": 1,
-         "visit_count": 1358,
-         "product_image": "test_image_iphone_13_pro_max_1.jpeg"
-         },
-    ]
-
     for user in users:
         customer = Customer()
         customer.username = user["username"]
@@ -233,23 +162,25 @@ def create_test_data():
         db.session.add(category)
         db.session.commit()
 
-    for prod in products:
-        product = Product()
-        product.product_name = prod["product_name"]
-        product.price = prod["price"]
-        product.discount = prod["discount"]
-        product.stock = prod["stock"]
-        product.size = prod["size"]
-        product.size_units = prod["size_units"]
-        product.weight = prod["weight"]
-        product.weight_units = prod["weight_units"]
-        product.color = prod["color"]
-        product.subheading = prod["subheading"]
-        product.description = prod["description"]
-        product.brand_id = prod["brand_id"]
-        product.category_id = prod["category_id"]
-        product.visit_count = prod["visit_count"]
-        product.product_image = prod["product_image"]
-        db.session.add(product)
-        db.session.commit()
+    with open('myshop/products.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            product = Product()
+            product.product_name = row["product_name"]
+            product.price = row["price"]
+            product.discount = row["discount"]
+            product.stock = row["stock"]
+            product.size = row["size"]
+            product.size_units = row["size_units"]
+            product.weight = row["weight"]
+            product.weight_units = row["weight_units"]
+            product.color = row["color"]
+            product.subheading = row["subheading"]
+            product.description = row["description"]
+            product.brand_id = row["brand_id"]
+            product.category_id = row["category_id"]
+            product.visit_count = row["visit_count"]
+            product.product_image = row["product_image"]
+            db.session.add(product)
+            db.session.commit()
     return render_template("auth.html", customer=current_user)
