fbd861d6de4412d2450c0c30f602d8170ae7fee0
liborhavranek <liborhavranek91@gmai.com>
2023-05-15 21:45:34 +0200

    Add to product next parameters
    -  update tests and make it faster
    
    see #36

--- myshop/tests/my_test_mixin.py
+++ myshop/tests/my_test_mixin.py
@@ -12,15 +12,6 @@ class TestMixin:
     def start_test(self):
         self.start_time = time.monotonic()
         print("\033[91mStarting test......................................................................\033[0m")
-        # time.sleep(0.2)
-        total_width = 70  # Set the total width of the progress bar
-        for i in range(8):
-            percent_complete = (i + 1) * 12.5
-            num_hashes = round(percent_complete * total_width / 100)
-            progress_bar = "#" * num_hashes + "." * (total_width - num_hashes)
-            sys.stdout.write(f"\rTesting {progress_bar} {percent_complete:>3} %")
-            sys.stdout.flush()
-            # time.sleep(0.1)
         os.environ.pop('FLASK_ENV', None)
 
         if self._outcome.success:
@@ -33,7 +24,7 @@ class TestMixin:
                 f"\nRunning test: {self.test_name} - "
                 f"{self._testMethodName} - \033[31mFAILED\033[0m")
 
-        time.sleep(0.1)
+
         if self._outcome.success:
             print("\033[32m" + f"{self._testMethodName} - passed." + "\033[0m")
             print("\033[32m" + f"{self._testMethodName} - completed.\n\n" + "\033[0m")
@@ -41,9 +32,6 @@ class TestMixin:
         else:
             print("\033[31m" + f"{self._testMethodName} - failed." + "\033[0m")
             print("\033[31m" + f"{self._testMethodName} - completed.\n\n" + "\033[0m")
-
-        # time.sleep(0.8)
-
 
     def setUp(self):
         super().setUp()
