09903f5a1d7a6c1473b5068ed16c09de6c7acaaa
liborhavranek <liborhavranek91@gmai.com>
2023-05-16 23:25:37 +0200

    Edit mobile product
    -  form for add and edit notebook is complete
    
    see #37

--- myshop/forms/add_notebook_form.py
+++ myshop/forms/add_notebook_form.py
@@ -6,7 +6,6 @@ from flask_wtf.file import FileAllowed
 from wtforms import StringField, SubmitField, TextAreaField, SelectField, DecimalField, IntegerField, HiddenField, \
     FileField, MultipleFileField, BooleanField
 from wtforms.validators import InputRequired, Length, Optional, NumberRange, ValidationError
-
 from myshop.models.brand_model import Brand
 from myshop.models.category_model import Category
 from myshop.models.product_model import Product
