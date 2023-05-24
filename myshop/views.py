3f65deb45b19bb7fbe1a8eea2a4549b10124d78f
liborhavranek <liborhavranek91@gmai.com>
2023-05-24 11:12:17 +0200

    Edit mobile product
    - create data working good
    
    see #38

--- myshop/views.py
+++ myshop/views.py
@@ -2,9 +2,17 @@
 
 from flask import Blueprint, render_template
 
+from myshop import db
+from myshop.models.category_model import Category
+from myshop.models.product_model import Product
+
 views = Blueprint('views', __name__, template_folder='templates')
 
 
 @views.route('/')
 def view() -> str:
-    return render_template('views.html')
+    categories = db.session.query(Category.category_name.distinct()).all()
+    products = Product.query.order_by(Product.date_created.desc()).all()
+    newest_products = Product.query.order_by(Product.date_created.desc()).limit(5).all()
+    return render_template('views.html', categories=categories, products=products, newest_products=newest_products)
+
